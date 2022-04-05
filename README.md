415eff55a11bd7457562e251368e2e5d0f5bf189
sanji950 <yu_dai26@yahoo.com.tw>
2022-04-05 14:51:41 +0800

    V1

---
+++ README.md
@@ -0,0 +1,44 @@
+# 介紹
+
+紀錄餐廳清單，可以瀏覽餐廳、查看餐廳詳細資訊。
+
+
+## 功能
+
+查看所有餐廳
+瀏覽餐廳的詳細資訊
+搜尋特定餐廳
+
+
+## 專案安裝流程
+
+1. 安裝 node.js 與 npm
+
+2. clone 此專案至本地
+
+3. 由終端機進入資料夾, 並輸入:
+```
+ npm install
+```
+
+4. 安裝完成後, 再輸入:
+```
+npm run star
+```
+
+5. 出現以下訊息代表順利運行, 並用瀏覽器進入localhost:3000
+```
+Express is listening on localhost:3000
+```
+
+6. 暫停使用, 請在終端機輸入以下內容:
+```
+ctrl + c
+```
+
+## 開發工具
+-Node.js 16.14.2
+-Express 4.16.4
+-Express-Handlebars 3.0.0
+-Bootstrap 4.3.1
+-Font-awesome 5.8.1
---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// require express-handlebars
+const exphbs = require('express-handlebars')
+
+//require restaurantList API
+const restaurantList = require('./restaurant.json')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+// setting static files
+app.use(express.static('public'))
+
+
+// routes setting >> index
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+// routes setting >> show
+app.get('/restaurants/:restaurantId', (req, res) => {
+  const restaurantData = restaurantList.results.find(item => item.id.toString() === req.params.restaurantId)
+  console.log(restaurantList)
+  res.render('show', { restaurant: restaurantData })
+})
+
+// routes setting >> search
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.toLocaleLowerCase().trim()
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLocaleLowerCase().includes(keyword)
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+// listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
+
